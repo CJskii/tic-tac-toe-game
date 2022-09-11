b7a7c9d61ecd922b971556db13aee1a4f007f31e
CJskii <czareczarek3@gmail.com>
2022-09-11 17:09:59 +0100

    game plan

---
+++ script.js
@@ -0,0 +1,23 @@
+// Event lisener for player selection
+// Board Array
+// Win conditions
+
+// 1. Player selects X or O
+// -- start turn function
+// 2. Player click on the board
+// 3. Player selection is recorded 
+// 4. Computer turn 
+// 5. Computer selection recorded
+// check for win conditions -- end of turn
+// 6. Print winner
+
+
+
+
+
+/* 
+      BOARD
+    | 0 1 2 |
+    | 3 4 5 |
+    | 6 7 8 |
+*/
