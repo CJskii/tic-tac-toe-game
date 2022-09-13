321e62af0cb625cee1e7ed3d6958571e48ed4d53
CJskii <czareczarek3@gmail.com>
2022-09-13 23:29:41 +0100

    announceWinner()

--- script.js
+++ script.js
@@ -90,36 +90,54 @@ document.addEventListener('DOMContentLoaded', () =>{
       }
     const arrayXsorted = arrayX.slice().sort()
     const arrayOsorted = arrayO.slice().sort()
-    compareArray(arrayXsorted, winCondition)
-    compareArray(arrayOsorted, winCondition)
+    compareArray(arrayXsorted, winCondition, "X")
+    compareArray(arrayOsorted, winCondition, "O")
   }
 
-  function compareArray(arr1, arr2){
-    let condition
-    for (i = 0; i < arr2.length; i++){
-      condition = arr2[i]
-      let winnerArray = []
-    for(j = 0; j < arr1.length; j++){
-      let number = arr1[j]
-      let a = condition[0]
-      let b = condition[1]
-      let c = condition[2]
-      if (number == a || number == b || number == c){
-        winnerArray.push(number)
-        if (winnerArray.length == '3'){
-          console.log(condition)
-          console.log("true")
-          return true;
-        } else {
-          console.log("No match")
-        }       
+  function compareArray(arr1, arr2, player){
+      let condition
+      for (i = 0; i < arr2.length; i++){
+        condition = arr2[i]
+        let winnerArray = []
+      for(j = 0; j < arr1.length; j++){
+        let number = arr1[j]
+        let a = condition[0]
+        let b = condition[1]
+        let c = condition[2]
+        if (number == a || number == b || number == c){
+          winnerArray.push(number)
+          if (winnerArray.length == '3'){
+            //console.log(condition)
+            announceWinner(player)
+            //console.log("true")
+            return true;
+          } else {
+            console.log("No match")
+            announceWinner("tie")
+          }       
+        }
       }
     }
   }
+
+  function announceWinner(player){
+    if (player == "X"){
+      winner.textContent = PLAYERX_WON
+    } else if (player == "O"){
+      winner.textContent = PLAYERO_WON
+    } else if (player == "tie"){
+      winner.textContent = TIE
+    }
+    return;
   }
   
 
 } )
+
+
+
+
+
 
 let arr1 = [0, 2 , 4 , 6 , 8]
 let arr2 = [
